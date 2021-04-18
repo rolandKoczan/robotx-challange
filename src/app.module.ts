2592d41a399fe21a93aea564c4d701b0d4a0a3c4
rolandKoczan <roland.koczan@gmail.com>
2021-04-18 15:23:21 +0200

    Restored default app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common"
+import { GraphQLModule } from "@nestjs/graphql"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { join } from "path"
+import { ParentModule } from "./parent/parent.module"
+import { ChildModule } from "./child/child.module"
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ParentModule,
+        ChildModule,
+        GraphQLModule.forRoot({
+            typePaths: ["./**/*.graphql"],
+            definitions: {
+                path: join(process.cwd(), "src/graphql.ts"),
+            },
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
