2ff83740986677bb1c03ee1877dd98287c2fc23e
rolandKoczan <roland.koczan@gmail.com>
2021-04-18 15:24:18 +0200

    Added mock database

--- src/child/child.resolver.ts
+++ src/child/child.resolver.ts
@@ -1,9 +1,11 @@
-import { Resolver, Query, Args } from "@nestjs/graphql"
+import { Logger } from "@nestjs/common";
+import { Resolver, Query, Args, ResolveField } from "@nestjs/graphql"
 import { Child } from "src/graphql"
 import { ChildService } from "./child.service"
 
-@Resolver()
+@Resolver('Child')
 export class ChildResolver {
+    private readonly logger = new Logger(ChildResolver.name);
     constructor(private readonly childService: ChildService) {}
 
     @Query("child")
--- src/child/child.service.ts
+++ src/child/child.service.ts
@@ -1,12 +1,24 @@
-import { Injectable } from "@nestjs/common"
-import { Child } from "../graphql"
+import { Injectable, Logger } from "@nestjs/common"
+import { A, B, Child } from "../graphql"
 
 @Injectable()
 export class ChildService {
-    private readonly children: Array<Child> = []
+    private readonly logger = new Logger(ChildService.name)
+    private readonly children: Array<any> = [
+        { _id: "1", name: "Child 1", parentId: "1", foo: "foo" },
+        { _id: "2", name: "Child 2", parentId: "1", bar: "bar" },
+        { _id: "3", name: "Child 3", parentId: "2", foo: "foo" },
+        { _id: "4", name: "Child 4", parentId: "3", foo: "foo" },
+        { _id: "5", name: "Child 5", parentId: "3", foo: "foo" },
+        { _id: "6", name: "Child 6", parentId: "3", bar: "foo" },
+    ]
 
-    findAll(): Child[] {
-        return this.children
+    findAllByParentId(_id: number): Array<any> {
+        const result = this.children.filter(
+            (child) => child.parentId === _id.toString()
+        )
+        result.forEach((i) => this.logger.debug(i))
+        return result
     }
 
     findOneById(id: string): Child {
